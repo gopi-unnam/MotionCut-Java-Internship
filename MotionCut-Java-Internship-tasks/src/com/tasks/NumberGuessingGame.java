5dcbe05760c2974f3a75226ff7372680125cc0b9
Administrator <Administrator@GopiUnnam>
2023-12-01 23:45:02 +0530

    Internship-tasks

---
+++ MotionCut-Java-Internship-tasks/src/com/tasks/NumberGuessingGame.java
@@ -0,0 +1,38 @@
+package com.tasks;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I have selected a number between 1 and 100. Try to guess it.");
+
+        int lowerBound = 1;
+        int upperBound = 100;
+        int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+
+        int userGuess;
+        int attempts = 0;
+
+        do {
+            System.out.print("Enter your guess: ");
+            userGuess = scanner.nextInt();
+            attempts++;
+
+            if (userGuess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > randomNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+            }
+        } while (userGuess != randomNumber);
+
+        scanner.close();
+    }
+}
---
+++ MotionCut-Java-Internship-tasks/src/com/tasks/SimpleCalculator.java
@@ -0,0 +1,57 @@
+package com.tasks;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Simple Calculator");
+        System.out.println("Supported operations: +, -, *, /");
+
+     
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+       
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Choose the operation (+,-,*,/): ");
+        char operation = scanner.next().charAt(0);
+
+        double result = calculate(num1, num2, operation);
+        System.out.println("Result: " + result);
+
+        scanner.close();
+    }
+
+    private static double calculate(double num1, double num2, char operation) {
+        double result = 0;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    System.exit(1); 
+                }
+                break;
+            default:
+                System.out.println("Error: Unsupported operation.");
+                System.exit(1); 
+        }
+
+        return result;
+    }
+}
